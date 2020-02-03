f47d57185fb748ff4d8d21d349e6927dca4fec15
Manabu Miwa <manabu.miwa@gcl0503m-miwa01.local>
2020-02-03 11:12:09 +0900

    Change some variable names to prevent name conflict

--- ecl/resource_ecl_rca_user_v1_test.go
+++ ecl/resource_ecl_rca_user_v1_test.go
@@ -24,20 +24,20 @@ func TestAccRCAV1User_basic(t *testing.T) {
 			{
 				Config: testAccRCAV1UserBasic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckRCAV1UserExists(resourcePath, &user),
-					resource.TestCheckResourceAttr(resourcePath, "password", "dummy_passw@rd"),
-					resource.TestCheckResourceAttrSet(resourcePath, "name"),
-					resource.TestCheckResourceAttrSet(resourcePath, "vpn_endpoints.0.endpoint"),
-					resource.TestCheckResourceAttrSet(resourcePath, "vpn_endpoints.0.type"),
+					testAccCheckRCAV1UserExists(testAccRCAV1ResourcePath, &user),
+					resource.TestCheckResourceAttr(testAccRCAV1ResourcePath, "password", "dummy_passw@rd"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "name"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.endpoint"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.type"),
 				),
 			},
 			{
 				Config: testAccRCAV1UserUpdate,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourcePath, "password", "dummy_passw@rd_updated"),
-					resource.TestCheckResourceAttrSet(resourcePath, "name"),
-					resource.TestCheckResourceAttrSet(resourcePath, "vpn_endpoints.0.endpoint"),
-					resource.TestCheckResourceAttrSet(resourcePath, "vpn_endpoints.0.type"),
+					resource.TestCheckResourceAttr(testAccRCAV1ResourcePath, "password", "dummy_passw@rd_updated"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "name"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.endpoint"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.type"),
 				),
 			},
 		},
@@ -102,17 +102,17 @@ func testAccCheckRCAV1UserExists(n string, user *users.User) resource.TestCheckF
 	}
 }
 
-var randomName = acctest.RandStringFromCharSet(16, acctest.CharSetAlphaNum)
-var resourcePath = fmt.Sprintf("ecl_rca_user_v1.%s", randomName)
+var testAccRCAV1RandomName = acctest.RandStringFromCharSet(16, acctest.CharSetAlphaNum)
+var testAccRCAV1ResourcePath = fmt.Sprintf("ecl_rca_user_v1.%s", testAccRCAV1RandomName)
 
 var testAccRCAV1UserBasic = fmt.Sprintf(`
 resource "ecl_rca_user_v1" "%s" {
     password = "dummy_passw@rd"
 }
-`, randomName)
+`, testAccRCAV1RandomName)
 
 var testAccRCAV1UserUpdate = fmt.Sprintf(`
 resource "ecl_rca_user_v1" "%s" {
     password = "dummy_passw@rd_updated"
 }
-`, randomName)
+`, testAccRCAV1RandomName)
