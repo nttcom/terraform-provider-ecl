960ce9b654e5d4aa6e5eb9280d1a17c5d5a9b1ce
Manabu Miwa <manabu.miwa@gcl0503m-miwa01.local>
2020-02-03 14:34:24 +0900

    Fix ambiguous variable names

--- ecl/resource_ecl_rca_user_v1_test.go
+++ ecl/resource_ecl_rca_user_v1_test.go
@@ -24,20 +24,20 @@ func TestAccRCAV1User_basic(t *testing.T) {
 			{
 				Config: testAccRCAV1UserBasic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckRCAV1UserExists(testAccRCAV1ResourcePath, &user),
-					resource.TestCheckResourceAttr(testAccRCAV1ResourcePath, "password", "dummy_passw@rd"),
-					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "name"),
-					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.endpoint"),
-					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.type"),
+					testAccCheckRCAV1UserExists(testAccRCAV1UserResourcePath, &user),
+					resource.TestCheckResourceAttr(testAccRCAV1UserResourcePath, "password", "dummy_passw@rd"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1UserResourcePath, "name"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1UserResourcePath, "vpn_endpoints.0.endpoint"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1UserResourcePath, "vpn_endpoints.0.type"),
 				),
 			},
 			{
 				Config: testAccRCAV1UserUpdate,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(testAccRCAV1ResourcePath, "password", "dummy_passw@rd_updated"),
-					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "name"),
-					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.endpoint"),
-					resource.TestCheckResourceAttrSet(testAccRCAV1ResourcePath, "vpn_endpoints.0.type"),
+					resource.TestCheckResourceAttr(testAccRCAV1UserResourcePath, "password", "dummy_passw@rd_updated"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1UserResourcePath, "name"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1UserResourcePath, "vpn_endpoints.0.endpoint"),
+					resource.TestCheckResourceAttrSet(testAccRCAV1UserResourcePath, "vpn_endpoints.0.type"),
 				),
 			},
 		},
@@ -102,17 +102,17 @@ func testAccCheckRCAV1UserExists(n string, user *users.User) resource.TestCheckF
 	}
 }
 
-var testAccRCAV1RandomName = acctest.RandStringFromCharSet(16, acctest.CharSetAlphaNum)
-var testAccRCAV1ResourcePath = fmt.Sprintf("ecl_rca_user_v1.%s", testAccRCAV1RandomName)
+var testAccRCAV1UserRandomName = acctest.RandStringFromCharSet(16, acctest.CharSetAlphaNum)
+var testAccRCAV1UserResourcePath = fmt.Sprintf("ecl_rca_user_v1.%s", testAccRCAV1UserRandomName)
 
 var testAccRCAV1UserBasic = fmt.Sprintf(`
 resource "ecl_rca_user_v1" "%s" {
     password = "dummy_passw@rd"
 }
-`, testAccRCAV1RandomName)
+`, testAccRCAV1UserRandomName)
 
 var testAccRCAV1UserUpdate = fmt.Sprintf(`
 resource "ecl_rca_user_v1" "%s" {
     password = "dummy_passw@rd_updated"
 }
-`, testAccRCAV1RandomName)
+`, testAccRCAV1UserRandomName)
