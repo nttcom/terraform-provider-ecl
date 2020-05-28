aadfa7e041804c6a0e95f4820330be44c8acccd7
Sebastien Vincent <sebastien.vincent@cppextrem.com>
2020-05-28 05:09:06 +0200

    Adds cloud_drive attribute. (#60)
    * Adds cloud_drive attribute.
    
    * Adds config_drive attribute value to the State of Resource.
    
    * Upgrade eclcloud package to v1.9.1.
    
    * Fixes config_drive parameter to be a boolean instead of string.
    
    Co-authored-by: shimisho <45929740+shimisho@users.noreply.github.com>
    
    * Refines go.sum.
    
    Co-authored-by: shimisho <45929740+shimisho@users.noreply.github.com>

--- ecl/resource_ecl_compute_instance_v2.go
+++ ecl/resource_ecl_compute_instance_v2.go
@@ -85,6 +85,12 @@ func resourceComputeInstanceV2() *schema.Resource {
 					}
 				},
 			},
+			"config_drive": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+				Default:  false,
+			},
 			"availability_zone": &schema.Schema{
 				Type:             schema.TypeString,
 				Optional:         true,
@@ -271,6 +277,8 @@ func resourceComputeInstanceV2Create(d *schema.ResourceData, meta interface{}) e
 	// Build a []servers.Network to pass into the create options.
 	networks := expandInstanceNetworks(allInstanceNetworks)
 
+	configDrive := d.Get("config_drive").(bool)
+
 	createOpts = &servers.CreateOpts{
 		Name:             d.Get("name").(string),
 		ImageRef:         imageID,
@@ -279,6 +287,7 @@ func resourceComputeInstanceV2Create(d *schema.ResourceData, meta interface{}) e
 		Networks:         networks,
 		Metadata:         resourceInstanceMetadataV2(d),
 		UserData:         []byte(d.Get("user_data").(string)),
+		ConfigDrive:      &configDrive,
 	}
 
 	if keyName, ok := d.Get("key_pair").(string); ok && keyName != "" {
@@ -455,6 +464,12 @@ func resourceComputeInstanceV2Read(d *schema.ResourceData, meta interface{}) err
 		d.Set("power_state", currentStatus)
 	default:
 		return fmt.Errorf("Invalid power_state for instance %s: %s", d.Id(), server.Status)
+	}
+
+	if server.ConfigDrive == "True" {
+		d.Set("config_drive", true)
+	} else {
+		d.Set("config_drive", false)
 	}
 
 	return nil
