4aa8a67ea573f51da95e3700c57ba5a547f6ecc9
AkiRa <ak1ra24net@gmail.com>
2021-03-09 09:16:54 +0900

    Support qos_options in data source (#76)
    * Support qos_options in data source
    
    * add qos_options data_source doc
    
    * Fix typo and the name, ID, description, ... of the mock response
    
    * fix indent
    
    * delete testAccNetworkV2QosOptionsDataSourceDescription
    
    * tested using description as mock only
    
    * fix 20M-GA-AZURE to 10Mbps-BestEffort
    
    * fix 10m to 10M

---
+++ ecl/data_source_ecl_network_qos_options_v2_mock_test.go
@@ -0,0 +1,166 @@
+package ecl
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/nttcom/terraform-provider-ecl/ecl/testhelper/mock"
+)
+
+func TestMockedAccNetworkV2QosOptionsDataSource_basic(t *testing.T) {
+	mc := mock.NewMockController()
+	defer mc.TerminateMockControllerSafety()
+
+	postKeystone := fmt.Sprintf(fakeKeystonePostTmpl, mc.Endpoint(), OS_REGION_NAME)
+	mc.Register(t, "keystone", "/v3/auth/tokens", postKeystone)
+	mc.Register(t, "qos_options", "/v2.0/qos_options", testMockNetworkV2QosOptionsListNameQuery)
+
+	mc.StartServer(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_1"),
+					resource.TestCheckResourceAttr(
+						"data.ecl_network_qos_options_v2.qos_options_1", "name", "10Mbps-BestEffort"),
+				),
+			},
+		},
+	})
+}
+
+func TestMockedAccNetworkV2QosOptionsDataSource_queries(t *testing.T) {
+	mc := mock.NewMockController()
+	defer mc.TerminateMockControllerSafety()
+
+	postKeystone := fmt.Sprintf(fakeKeystonePostTmpl, mc.Endpoint(), OS_REGION_NAME)
+	mc.Register(t, "keystone", "/v3/auth/tokens", postKeystone)
+	mc.Register(t, "qos_options", "/v2.0/qos_options", testMockNetworkV2QosOptionsListNameQuery)
+	mc.Register(t, "qos_options", "/v2.0/qos_options", testMockNetworkV2QosOptionsListIDQuery)
+	mc.Register(t, "qos_options", "/v2.0/qos_options", testMockNetworkV2QosOptionsListDescriptionQuery)
+
+	mc.StartServer(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceName,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_1"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceID,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_2"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceDescription,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_1"),
+				),
+			},
+		},
+	})
+}
+
+var testMockNetworkV2QosOptionsListNameQuery = `
+request:
+    method: GET
+    query:
+      name:
+        - 10Mbps-BestEffort
+response: 
+    code: 200
+    body: >
+        {
+            "qos_options": [
+                {
+                    "aws_service_id": null,
+                    "azure_service_id": null,
+                    "bandwidth": "10",
+                    "description": "10M-besteffort-menu",
+                    "fic_service_id": null,
+                    "gcp_service_id": null,
+                    "id": "a6b91294-8870-4f2c-b9e9-a899acada723",
+                    "interdc_service_id": null,
+                    "internet_service_id": null,
+                    "name": "10Mbps-BestEffort",
+                    "qos_type": "besteffort",
+                    "service_type": "internet",
+                    "status": "ACTIVE",
+                    "vpn_service_id": null
+                }
+            ]
+        }
+`
+
+var testMockNetworkV2QosOptionsListIDQuery = `
+request:
+    method: GET
+    query:
+      id:
+        - a6b91294-8870-4f2c-b9e9-a899acada723
+response: 
+    code: 200
+    body: >
+        {
+            "qos_options": [
+                {
+                    "aws_service_id": null,
+                    "azure_service_id": null,
+                    "bandwidth": "10",
+                    "description": "10M-besteffort-menu",
+                    "fic_service_id": null,
+                    "gcp_service_id": null,
+                    "id": "a6b91294-8870-4f2c-b9e9-a899acada723",
+                    "interdc_service_id": null,
+                    "internet_service_id": null,
+                    "name": "10Mbps-BestEffort",
+                    "qos_type": "besteffort",
+                    "service_type": "internet",
+                    "status": "ACTIVE",
+                    "vpn_service_id": null
+                }
+            ]
+        }
+`
+
+var testMockNetworkV2QosOptionsListDescriptionQuery = `
+request:
+    method: GET
+    query:
+      description:
+        - "10M-besteffort-menu"
+response: 
+    code: 200
+    body: >
+        {
+            "qos_options": [
+                {
+                    "aws_service_id": null,
+                    "azure_service_id": null,
+                    "bandwidth": "10",
+                    "description": "10M-besteffort-menu",
+                    "fic_service_id": null,
+                    "gcp_service_id": null,
+                    "id": "a6b91294-8870-4f2c-b9e9-a899acada723",
+                    "interdc_service_id": null,
+                    "internet_service_id": null,
+                    "name": "10Mbps-besteffort",
+                    "qos_type": "besteffort",
+                    "service_type": "internet",
+                    "status": "active",
+                    "vpn_service_id": null
+                }
+            ]
+        }
+`
---
+++ ecl/data_source_ecl_network_qos_options_v2_test.go
@@ -0,0 +1,98 @@
+package ecl
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccNetworkV2QosOptionsDataSource_basic(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skip this test in short mode")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_1"),
+					resource.TestCheckResourceAttr(
+						"data.ecl_network_qos_options_v2.qos_options_1", "name", "10Mbps-BestEffort"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNetworkV2QosOptionsDataSource_queries(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skip this test in short mode")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceName,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_1"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccNetworkV2QosOptionsDataSourceID,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkV2QosOptionsDataSourceID("data.ecl_network_qos_options_v2.qos_options_2"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckNetworkV2QosOptionsDataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find qos options data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Qos Options data source ID not set")
+		}
+
+		return nil
+	}
+}
+
+var testAccNetworkV2QosOptionsDataSourceBasic = `
+data "ecl_network_qos_options_v2" "qos_options_1" {
+    name = "10Mbps-BestEffort"
+}
+`
+
+var testAccNetworkV2QosOptionsDataSourceID = `
+data "ecl_network_qos_options_v2" "qos_options_1" {
+    name = "10Mbps-BestEffort"
+}
+
+data "ecl_network_qos_options_v2" "qos_options_2" {
+  qos_option_id = "${data.ecl_network_qos_options_v2.qos_options_1.id}"
+}
+`
+
+var testAccNetworkV2QosOptionsDataSourceName = `
+data "ecl_network_qos_options_v2" "qos_options_1" {
+  name = "10Mbps-BestEffort"
+}
+`
+
+var testAccNetworkV2QosOptionsDataSourceDescription = `
+data "ecl_network_qos_options_v2" "qos_options_1" {
+    description = "10M-besteffort-menu"
+}
+`
